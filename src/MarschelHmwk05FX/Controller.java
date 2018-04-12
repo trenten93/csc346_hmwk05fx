0a44f84d758db312e582afbc29a50cdced30ef0a
trenten93 <35821142+trenten93@users.noreply.github.com>
2018-04-11 19:12:13 -0500

    I think it's close to being finished

--- src/MarschelHmwk05FX/Controller.java
+++ src/MarschelHmwk05FX/Controller.java
@@ -4,16 +4,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
+import java.awt.*;
 import java.sql.*;
 import java.util.*;
 import java.io.*;
@@ -25,10 +30,29 @@ import sun.security.validator.ValidatorException;
 import javax.sound.midi.ControllerEventListener;
 import java.net.*;
 import java.net.MalformedURLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Window;
+
 
 
 public class Controller{
     public static String key = WeatherDriver.getKey();
+
+    public String tempState= "";
+
+    public String[] statesArray = {"AK", "AL", "AR", "AZ", "CA", "CO", "CT",
+            "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY",
+            "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND",
+            "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI",
+            "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"};
+
+
 
     @FXML
     public Button zipSearchButton;
@@ -40,9 +64,6 @@ public class Controller{
     public TextField zipField;
 
     @FXML
-    public TextField stateField;
-
-    @FXML
     public TextField cityField;
 
     @FXML
@@ -50,6 +71,21 @@ public class Controller{
 
     @FXML
     public Button citySearchButton;
+
+    @FXML
+    public ComboBox<String> stateComboBox;
+
+    @FXML
+    public TabPane mainWindow;
+
+    @FXML
+    public Tab cityTab;
+
+    @FXML
+    public ImageView image;
+
+
+
 
 
 
@@ -76,7 +112,7 @@ public class Controller{
     }
 
     public void citySearchButtonRun(){
-        String state = stateField.getText();
+        String state = stateComboBox.getValue().toString();
         String city = cityField.getText();
 
         String[] searchResults = WeatherDriver.getCityStateFromCityName(city,state);
@@ -96,6 +132,41 @@ public class Controller{
     }
 
 
+    public void cityTabOpened(){
+        stateComboBox.getItems().addAll(statesArray);
+        stateComboBox.setVisibleRowCount(13);
+
+
+    }
+
+    public void zipEnter(){
+        if(zipField.getText().length() == 5){
+            zipSearchButtonRun();
+        }
+    }
+
+    public void cityEnter(){
+        if(cityField.getText().length()>0){
+            citySearchButtonRun();
+        }
+    }
+
+    public void imageClick(){
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI oURL = new URI("http://www.wunderground.com");
+            desktop.browse(oURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showList(){
+        stateComboBox.show();
+    }
+    public void hideList(){
+        stateComboBox.hide();
+    }
 
 
 
